b3703f7d1090afd35e7074810b9e01a2f212d284
Pedro Lucas <61566405+Pedro1516@users.noreply.github.com>
2023-03-24 03:41:00 -0300

    Update main.c

--- main/main.c
+++ main/main.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
 #include <string.h>
 
-struct clientes
+typedef  struct clientes
 {
+	char id[10];
 	char nome[30];
 	char idade[3];
 	char estadoCivil[10];
-};
+}add;
 
+//Adiciona os dados numa struct
+add adicionarDados(char name[], char age[], char status[]){
+	struct clientes novoCliente;
+	strcpy(novoCliente.nome, name);
+	strcpy(novoCliente.idade, age);
+	strcpy(novoCliente.estadoCivil, status);
+	
+	return novoCliente;
+}
 
 //Cria uma ficha de inscricao
 int criarInscricao(void)
@@ -45,23 +55,21 @@ int criarInscricao(void)
 			if(estadoCivil_selecao >= 0 && estadoCivil_selecao < 5){aux = 0;}
 		}
 		
+		add teste =  adicionarDados(nome, idade, estadoCivil);
+	  	printf("\n\nNome: %s\nIdade: %s\nEstado Civil: %s", teste.nome, teste.idade, teste.estadoCivil);
+				
 		return 0;
 }
 
-//Adiciona os dados numa struct
-int adicionarDados(){
-	
-}
 
 
-
-
+//funcao principal
 int main(void)
 {
-	printf("Olá, bem vindo ao SII(Sistema integado de inscrições \n\nVocê já tem uma conta?");
+	printf("Olá, bem vindo ao SII(Sistema integado de inscrições) \n\nVocê já tem uma conta?");
 	
 	int escolhaConta;
-	printf("\nSIM[1]  NAO[0]\n");
+	printf("\nSIM[1]  NÃO[0]\n");
 	scanf("%d", &escolhaConta);
 	
 	if(escolhaConta)
@@ -70,20 +78,9 @@ int main(void)
 	}
 	else
 	{
-		criarInscricao();
+	criarInscricao();
 	}
 	
-	//	printf("\n\nNome: %s\nIdade: %s\nEstado Civil: %s", nome, idade, estadoCivil);
 		
-	
-	
-	
-	
 	return 0;
 }
-
-
-//id: 000000
-//Nome: (nome)
-//Idade: (idade)
-//Estado Civil: (est civ)
