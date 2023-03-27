d4ab469e73f7ec123771c1dcd91e2a73b64ee759
Pedro Lucas <61566405+Pedro1516@users.noreply.github.com>
2023-03-27 03:49:21 -0300

    Update main.c

--- main/main.c
+++ main/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 #include <sqlite3.h>
+#include <stdlib.h>
+#include <string.h>
 
 //funcao callback, ela alinha os itens da tabela
 static int callback(void *NotUsed, int argc, char **argv, char **azColName)
@@ -28,41 +30,37 @@ int criarTabela()
 	sqlite3 *db;
 	int exit = 0;
 	char *sql;
-	char *messageError;
+	char *messageError = NULL;
 
 	//Abre o banco de dados
 	exit = sqlite3_open("Banco de Dados/lista_clientes.db", &db);
-	
-	if(exit != SQLITE_OK){
+
+	if (exit != SQLITE_OK)
+	{
 		printf("Erro ao abrir banco de dados");
 		sqlite3_close(db);
-		return 1;	
+		return 1;
 	}
-	
-	//verifica se a tabela Cliente já foi criada
-	exit = sqlite3_exec(db, "SELECT name FROM sqlite_master WHERE type='table' AND name='CLIENTE';", 0, 0, &messageError);
 
-	//se não, cria ela
-	if (exit != SQLITE_ROW)
+	//comando em sql para criação de tabela(A tabela só será criada se ela ainda não existir )
+	sql = "CREATE  TABLE IF NOT EXISTS CLIENTE("
+		  "ID INT PRIMARY KEY  NOT NULL, "
+		  "NOME 	TEXT 	NOT NULL, "
+		  "IDADE 	INT	NOT NULL, "
+		  "ESTADO_CIVIL TEXT NOT NULL);";
+
+	//executa o comando de criação de tabela
+	exit = sqlite3_exec(db, sql, callback, 0, &messageError);
+
+	//verifica se houve algum erro
+	if (exit != SQLITE_OK)
 	{
-		sql = "CREATE  TABLE CLIENTE("
-			  "ID INT PRIMARY KEY  NOT NULL, "
-			  "NOME 	TEXT 	NOT NULL, "
-			  "IDADE 	INT	NOT NULL, "
-			  "ESTADO_CIVIL TEXT NOT NULL);";
-			  
-		//executa o comando de criação de tabela	
-		exit = sqlite3_exec(db, sql, callback, 0, &messageError);
-		
-		//verifica se houve algum erro
-		if (exit != SQLITE_OK)
-		{
-			printf("Erro: %s\n", messageError);
-			sqlite3_free(messageError);
-			sqlite3_close(db);
-			return 1;
-		}
+		printf("Erro: %s\n", messageError);
+		sqlite3_free(messageError);
+		sqlite3_close(db);
+		return 1;
 	}
+
 	sqlite3_close(db);
 	return 0;
 }
@@ -73,10 +71,10 @@ int adicionarDados(char name[], char age[], char status[])
 	sqlite3 *db;
 	int exit = 0;
 	int id = 0;
-	char *messageError;
+	char *messageError = NULL;
 	char *sql;
 	char *query = "SELECT * FROM CLIENTE";
-	char *insert[50];
+	char insert[100];
 	char *count = "SELECT COUNT (*) FROM CLIENTE";
 
 	//inicia o banco de dados
@@ -116,15 +114,22 @@ int adicionarDados(char name[], char age[], char status[])
 	return 0;
 }
 
-
 //Cria uma ficha de inscricao
 int criarInscricao(void)
-{	
-	char nome[10], idade[3], estadoCivil[10];
+{
+	char nome[50], idade[3], estadoCivil[10];
 	int estadoCivil_selecao, aux = 1;
-	
+
 	printf("\nPara começar, diga seu nome: ");
-	scanf("%s", nome);
+	//scanf("%s", nome);
+	getchar();
+	fgets(nome, sizeof(nome), stdin);
+
+	//Retira a quebra de linha(\n) da variável nome
+	if (nome[strlen(nome) - 1] == '\n')
+	{
+		nome[strlen(nome) - 1] = '\0';
+	}
 
 	printf("\nAgora diga sua idade: ");
 	scanf("%s", idade);
@@ -159,7 +164,49 @@ int criarInscricao(void)
 	}
 
 	adicionarDados(nome, idade, estadoCivil);
-	
+
+	return 0;
+}
+
+int buscarFicha(int id, char senha[])
+{
+	sqlite3 *db;
+	char *sql;
+	char *messageError = NULL;
+	int exit = 0;
+
+	//abre o banco de dados
+	exit = sqlite3_open("Banco de Dados/lista_clientes.db", &db);
+	//verifica se houve algum erro
+	if (exit)
+	{
+		printf("Não foi possivel abrir o banco de dados");
+		sqlite3_close(db);
+		return 1;
+	}
+
+	//verifica se a senha está correta(senha padrão)
+	if (strcmp(senha, "0000"))
+	{
+		printf("SENHA INCORRETA!");
+		return 0;
+	}
+	else
+	{
+		//seleciona da tablea cliente o elemento que possui id 1
+		sql = sqlite3_mprintf("SELECT * FROM CLIENTE WHERE ID = %d ", id);
+		exit = sqlite3_exec(db, sql, callback, 0, &messageError);
+			
+		//se ocorrer algum erro a execução da função para e é printado o erro 
+		if(exit != SQLITE_OK){
+			printf("ERRO AO MOSTRAR DADOS \nErro: %s", messageError);
+			sqlite3_free(messageError);
+			sqlite3_close(db);
+			return 1;
+		}
+	}
+
+	sqlite3_close(db);
 	return 0;
 }
 
@@ -168,26 +215,38 @@ int main(void)
 {
 	printf("Olá, bem vindo ao SII(Sistema integado de inscrições) \n\nVocê já tem uma conta?");
 
-	int escolhaConta;
-	printf("\nSIM[1]  NÃO[0]\n");
-	scanf("%d", &escolhaConta);
-
 	criarTabela();
 
-	if (escolhaConta)
+	int escolhaConta;
+	int aux = 1;
+	int id_login;
+	char senha[8];
+	//fica em loop até o usuário escolher uma opção válida
+	while (aux)
 	{
-		adicionarDados("Pedro", "58", "Solteiro");
-		return 0;
+		printf("\nSIM[1]  NÃO[0]\n");
+		scanf("%d", &escolhaConta);
+
+		if (escolhaConta == 1 || escolhaConta == 0)
+		{
+			aux = 0;
+		}
+	}
+
+	if (!escolhaConta)
+	{
+		criarInscricao();
 	}
 	else
 	{
-		criarInscricao();
+		printf("Insira seu Id: ");
+		scanf("%d", &id_login);
+
+		printf("Digite sua senha: ");
+		scanf("%s", &senha);
+
+		buscarFicha(id_login, senha);
 	}
 
 	return 0;
 }
-
-//id: 000000
-//Nome: (nome)
-//Idade: (idade)
-//Estado Civil: (est civ)
