f8d616d82620f556654266f5646235c86a82a4b2
Pedro Lucas <61566405+Pedro1516@users.noreply.github.com>
2023-04-01 13:55:36 -0300

    Add files via upload

---
+++ main/header.h
@@ -0,0 +1,12 @@
+#ifndef FUNCOES
+#define FUNCOES
+
+int criarTabela();
+
+int adicionarDados(char name[], char age[], char status[], char senha[]);
+
+int criarInscricao();
+
+int buscarFicha(int id, char senha[]);
+
+#endif
